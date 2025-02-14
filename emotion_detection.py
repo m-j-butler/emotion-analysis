44b148d27a999e6a7d60a1cc9fdbb1f2f6792c64
m-j-butler <mattbutler28@gmail.com>
2025-02-14 12:39:38 -0500

    amends output of emotion_detector function in emotion_detection.py to extract emotions and their scores from raw text and finds dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,4 +9,22 @@ def emotion_detector(text_to_analyse):
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=input_json, headers=header)
 
-    return response.text
+    # Parse the response from the API
+    response_json = json.loads(response.text)
+
+    # Extract emotions and their scores
+    emotions_data = [
+        ('anger'  , response_json['emotionPredictions'][0]['emotion']['anger']),
+        ('disgust', response_json['emotionPredictions'][0]['emotion']['disgust']),
+        ('fear'   , response_json['emotionPredictions'][0]['emotion']['fear']),
+        ('joy'    , response_json['emotionPredictions'][0]['emotion']['joy']),
+        ('sadness', response_json['emotionPredictions'][0]['emotion']['sadness']),
+        ]
+    
+    # Find highest scoring emotion
+    dominant_emotion = max(emotions_data, key=lambda item: item[1])
+    emotions_data.append(('dominant_emotion', dominant_emotion[0]))
+
+    # Convert to dictionary and return
+    return dict(emotions_data)
+
