d9e1fde02775c1f8164f9a398444dd5dea6b1067
m-j-butler <mattbutler28@gmail.com>
2025-02-14 16:23:05 -0500

    minor tweaks to server.py to satisfy Pylint

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''Executing function initiates application of sentiment analysis, 
+to be executed over Flask channel and deployed on localhost:5000.'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -12,7 +15,7 @@ def emo_detector():
 
     input_text = request.args.get('textToAnalyze')
     response = emotion_detector(input_text)
-    
+
     output = f"For the given statement, the system response is\
     'anger': {response['anger']}, \
     'disgust': {response['disgust']}, \
@@ -20,11 +23,11 @@ def emo_detector():
     'joy': {response['joy']} and \
     'sadness': {response['sadness']}.\
     The dominant emotion is {response['dominant_emotion']}."
-    
-    # Handling user input of empty text 
+
+    # Handling user input of empty text
     if response['dominant_emotion'] is None:
         return 'Invalid text! Please try again!'
-    
+
     return output
 
 
@@ -37,5 +40,5 @@ def render_index_page():
 
 
 if __name__ == '__main__':
-    '''Executes Flask app and deploys it on localhost:5000'''
+    # Executes Flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0", port=5000)
