a1297e5ed43075e9637c16e9f5b12dbe192f9a45
m-j-butler <mattbutler28@gmail.com>
2025-02-14 13:35:09 -0500

    adds server.py file which creates Flask app

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    '''Receives text from the HTML interface and runs
+    emotion detection on it using the emotion_detector()
+    function. Output returns a dictionary of emotions
+    with their score.'''
+
+    input_text = request.args.get('text_to_analyse')
+
+    response = emotion_detector(input_text)
+
+    output = f"For the given statement, the system response is\
+    'anger': {response['anger']}, \
+    'disgust': {response['disgust']}, \
+    'fear': {response['fear']}, \
+    'joy': {response['joy']} and \
+    'sadness': {response['sadness']}.\
+    The dominant emotion is{response['dominant_emotion']}."
+
+
+@app.route("/")
+def render_index_page:
+    '''Initiates rendering of main application page
+    over the Flask channel.'''
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    '''Executes Flask app and deploys it on localhost:5000'''
+    app.run(host="0.0.0.0", port=5000)
